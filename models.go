3d381bf7d17ce82682bdc61d9f05f0c04ba2ff1c
DurlaVerder <vrrrr227@gmail.com>
2025-02-04 00:20:37 +0300

    ifx

--- models.go
+++ models.go
@@ -2,10 +2,9 @@ package models
 
 import "time"
 
-
 type PingResult struct {
-	ID                 int       `json:"id" db:"id"`
-	IPAddress          string    `json:"ip_address" db:"ip_address"`
-	PingTime           int       `json:"ping_time" db:"ping_time"`
-	DateSuccessfulPing time.Time `json:"date_successful_ping" db:"date_successful_ping"`
+	ID                 int       `json:"id"`
+	IPAddress          string    `json:"ip_address"`
+	PingTime           int       `json:"ping_time"`
+	DateSuccessfulPing time.Time `json:"date_successful_ping"`
 }
